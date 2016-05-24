836755f95c9f85bd4633b409e1268fd677d173c7
GurenDLotus <hilton.d.filemon@gmail.com>
2016-05-24 14:33:20 -0300

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ var config = require('./config');
 var mongoose = require('./mongoose');
 
 module.exports = function(){
-	var db = mongoose.connect('db://localhost/myapp'););
+	var db = mongoose.connect('db://localhost/myapp');
 	require('../models/allmodels.server.model');
 	return db;
 };
